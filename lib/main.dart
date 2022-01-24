68fa35bcb9d6fc86c8fd2909d311f61f2c1ef480
Harsh <harsh.here17@gmail.com>
2022-01-24 16:19:38 +0530

    project init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bloclearn/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Employee App Bloc',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Employee App Bloc'),
+          ),
+          body: const Home()),
+    );
+  }
+}
